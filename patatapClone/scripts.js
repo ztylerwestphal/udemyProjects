e495d55af63161bd517d370ab59dd3b019dd00ac
ztylerwestphal <ztylerwestphal@gmail.com>
2019-12-09 21:35:24 -0500

    Add files via upload

---
+++ patatapClone/scripts.js
@@ -0,0 +1,14 @@
+// //make 10 horizontal Circles
+// for (var i = 1; i < 11; i++) {
+//     new Path.Circle(new Point(i*100, 10), 10).fillColor = "blue";
+// }
+// //make 10 vertical Circles
+// for (var i = 1; i < 11; i++) {
+//     new Path.Circle(new Point(10, i*100), 10).fillColor = "blue";
+// }
+// for (var i = 0; i < 10; i++) {
+//   for (var i = 1; i < 11; i++) {
+//       new Path.Circle(new Point(i*100, 10), 10).fillColor = "blue";
+//   }
+//   path.moveTo(10, i*100);
+// }
